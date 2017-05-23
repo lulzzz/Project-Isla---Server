b15fc6e293677780c85b42d238100027685ac010
Chase Redmon <redmon.chase@gmail.com>
2017-05-22 22:34:58 -0400

    Code comments and reset events

--- Project Isla - Server/Program.cs
+++ Project Isla - Server/Program.cs
@@ -23,15 +23,35 @@ namespace Project_Isla___Server
         public Server() { }
 
         private static Socket listener;
-        public static ManualResetEvent mre = new ManualResetEvent(false);
+
+        //Reset event for send callback
+        public static ManualResetEvent sendReset = new ManualResetEvent(false);
+
+        //Reset event for disconnect callback
+        public static ManualResetEvent disconnectReset = new ManualResetEvent(false);
+
+        //Reset event for accept callback
+        public static ManualResetEvent acceptConnectionReset = new ManualResetEvent(false);
+
+        //Reset even for read callback
+        public static ManualResetEvent receiveReset = new ManualResetEvent(false);
+
+        //Size of data buffer
         const int BufferSize = 1024;
+
+        //Run server on port 50000
         const int port = 50000;
+
+        //The server is running
         bool isRunning = true;
+
+        //Start and end of message delimiters
         public const string beginningDelim = "<!--STARTMESSAGE-->";
         public const string endingDelim = "<!--ENDMESSAGE-->";
 
         class StateObject
         {
+            //Start object class
             public Socket workSocket = null;
             public byte[] buffer = new byte[BufferSize];
             public StringBuilder sb = new StringBuilder();
@@ -39,18 +59,25 @@ namespace Project_Isla___Server
 
         static bool isConnected(Socket s)
         {
+            //Code for polling the socket to see if we are connected or not
             return !((s.Poll(1000, SelectMode.SelectRead) && (s.Available == 0) || !s.Connected));
         }
 
         static string between(string message)
         {
+            //Extract the message between the start and end delimiters
             return Regex.Match(message, @"^<!--STARTMESSAGE-->(.*?)<!--ENDMESSAGE-->$").Groups[1].Value;
         }
 
         public static void StartServer()
         {
+            //Buffer for data
             byte[] buffer = new byte[1024];
+
+            //Start the server on port 50000
             IPEndPoint localPoint = new IPEndPoint(IPAddress.Any, port);
+
+            //TCP Socket server
             listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             try
@@ -59,19 +86,34 @@ namespace Project_Isla___Server
 
                 while (true)
                 {
-                    mre.Reset();
+                    //10 Connections in queue 
                     listener.Listen(10);
+
+                    //Begin to accept the connection
                     listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+
+                    //Wait for the conection to finish connecting
+                    acceptConnectionReset.WaitOne();
                 }
             }
             catch (SocketException se)
             {
+                //Is there soemthing else running on port 50000?
                 Console.WriteLine(string.Format("Is something else running on port {0}?", port));
                 Console.WriteLine(string.Format("\n{0}", se.InnerException));
             }
             catch (Exception e)
             {
+                //Misc exception
                 Console.WriteLine(e.InnerException);
+            }
+            finally
+            {
+                //Start the disconnect
+                listener.BeginDisconnect(false, new AsyncCallback(DisconnectCallback), listener);
+
+                //Wait for disconnect
+                disconnectReset.WaitOne();
             }
         }
 
@@ -79,14 +121,22 @@ namespace Project_Isla___Server
         {
             StateObject so = new StateObject();
 
+            //Create the message with beginning delim, message, and ending delim
             so.sb.Append(beginningDelim);
             so.sb.Append(message);
             so.sb.Append(endingDelim);
+
+            //Store the built message in a string
             string send = so.sb.ToString();
 
+            //Convert string to byte array
             byte[] byteData = Encoding.UTF8.GetBytes(send);
 
+            //Begin to send the message
             handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(sendCallback), handler);
+
+            //Wait until the message is sent
+            sendReset.WaitOne();
         }
 
         static void AcceptCallback(IAsyncResult ar)
@@ -95,12 +145,20 @@ namespace Project_Isla___Server
 
             if (listener != null)
             {
+                //End the accept
                 Socket handler = listener.EndAccept(ar);
-                mre.Set();
+
+                //Reset the accept event
+                acceptConnectionReset.Set();
 
                 StateObject so = new StateObject();
                 so.workSocket = handler;
+
+                //Begin to receive data
                 handler.BeginReceive(so.buffer, 0, BufferSize, 0, new AsyncCallback(readCallback), so);
+
+                //Wait until the data is received
+                receiveReset.WaitOne();
             }
         }
 
@@ -109,21 +167,26 @@ namespace Project_Isla___Server
             StateObject so = (StateObject)ar.AsyncState;
             Socket handler = so.workSocket;
 
+            //Check to see if the socket is connected or not
             if (!isConnected(handler))
             {
                 handler.Close();
                 return;
             }
 
+            //Read the data
             int read = handler.EndReceive(ar);
 
             if (read > 0)
             {
+                //Store received data
                 so.sb.Append(Encoding.UTF8.GetString(so.buffer, 0, read));
 
-                if (so.sb.ToString().Contains(endingDelim))
+                if (so.sb.ToString().Contains(endingDelim)) //Check to see if the message contains the ending delim
                 {
                     string send = string.Empty;
+
+                    //Get the message between the beginning delim and ending delim
                     string message = between(so.sb.ToString());
 
                     switch (message)
@@ -136,34 +199,66 @@ namespace Project_Isla___Server
                             break;
                     }
 
+                    //Create appropriate response message
                     send = string.Format("{0}{1}{2}", beginningDelim, send, endingDelim);
 
+                    //Convert send message to bytes
                     byte[] bytesToSend = Encoding.UTF8.GetBytes(send);
 
+                    //Echo data back to client
                     handler.BeginSend(bytesToSend, 0, bytesToSend.Length, SocketFlags.None, new AsyncCallback(sendCallback), so);
 
+                    //Wait until the message is sent
+                    sendReset.WaitOne();
                 }
                 else
                 {
+                    //Receive more data
                     handler.BeginReceive(so.buffer, 0, BufferSize, 0, new AsyncCallback(readCallback), so);
+
+                    //Wait until the data has been received
+                    receiveReset.WaitOne();
                 }
             }
             else
             {
                 handler.Close();
             }
+
+            //Reset the receive event
+            receiveReset.Set();
         }
 
         static void sendCallback(IAsyncResult ar)
         {
+            //End the send message task
             StateObject so = (StateObject)ar.AsyncState;
             Socket handler = so.workSocket;
 
             handler.EndSend(ar);
 
+            //Reset the send message event
+            sendReset.Set();
+
+            //Create object to begin to receive more data
             StateObject newSo = new StateObject();
             newSo.workSocket = handler;
+
+            //Begin to receive data
             handler.BeginReceive(newSo.buffer, 0, BufferSize, 0, new AsyncCallback(readCallback), newSo);
+
+            //Wait until the data has been received
+            receiveReset.WaitOne();
+        }
+
+        static void DisconnectCallback(IAsyncResult ar)
+        {
+            //Disconnect
+            Socket so = (Socket)ar.AsyncState;
+            so.EndDisconnect(ar);
+
+            //Reset the disconnection event
+            disconnectReset.Set();
         }
     }
 }
